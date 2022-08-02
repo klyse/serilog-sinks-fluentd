eadc6129541d86fd8ec833e17e6e52627c9cb162
klyse <klaus.pruenster@gmail.com>
2022-08-02 16:22:04 +0200

    added test client

---
+++ TestClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+Console.WriteLine("Hello, World!");
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Fluentd(host: "192.168.178.30", port: 1996)
+    .WriteTo.Console()
+    .CreateLogger();
+
+
+while (true)
+{
+    // test log big item
+    Log.Information("heya Test {@Whatever} Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipisci[ng] velit, sed quia non numquam [do] eius modi tempora inci[di]dunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum[d] exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? [D]Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? [33] At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+        new {
+            Stuff1 = "yo1",
+            Stuff2 = "yo2",
+            Stuff3 = "yo3",
+            Stuff4 = "yo4",
+            Stuff5 = "yo5",
+            Stuff6 = "yo6",
+        });
+
+}
