d3634112580f6a3fa4cc52aba981776e4ef743ce
Klyse <Klaus.Pruenster@gmail.com>
2022-08-02 16:36:08 +0200

    resolve deprecation warnings (#5)
    * fix deprecation warning of periodic batching
    
    * add more configuration options in "writeTo.Fluentd" method
    
    * dispose FluentdSink

--- src/Serilog.Sinks.Fluentd/LoggerConfigurationFluentdExtensions.cs
+++ src/Serilog.Sinks.Fluentd/LoggerConfigurationFluentdExtensions.cs
@@ -1,6 +1,8 @@
-﻿using Serilog.Configuration;
+using System;
+using Serilog.Configuration;
 using Serilog.Events;
 using Serilog.Sinks.Fluentd;
+using Serilog.Sinks.PeriodicBatching;
 
 namespace Serilog
 {
@@ -9,27 +11,46 @@ namespace Serilog
         private const string Host = "localhost";
         private const int Port = 24224;
         private const string Tag = "Tag";
-
+        private const int QueueLimit = 100000;
+        private static readonly TimeSpan DefaultPeriod = TimeSpan.FromSeconds(2.0);
+        
+        
+        private static readonly PeriodicBatchingSinkOptions DefaultBatchingOptions = new PeriodicBatchingSinkOptions
+        {
+            BatchSizeLimit = 50,
+            QueueLimit = QueueLimit,
+            Period = DefaultPeriod
+        };
+        
         public static LoggerConfiguration Fluentd(
             this LoggerSinkConfiguration loggerSinkConfiguration,
             FluentdSinkOptions option = null,
+            PeriodicBatchingSinkOptions batchingOptions = null,
             LogEventLevel restrictedToMinimumLevel = LogEventLevel.Information)
         {
             var sink = new FluentdSink(option ?? new FluentdSinkOptions(Host, Port, Tag));
+            batchingOptions = batchingOptions ?? DefaultBatchingOptions;
 
-            return loggerSinkConfiguration.Sink(sink, restrictedToMinimumLevel);
+            var batchingSink = new PeriodicBatchingSink(sink, batchingOptions);
+            return loggerSinkConfiguration.Sink(batchingSink, restrictedToMinimumLevel);
         }
 
         public static LoggerConfiguration Fluentd(
            this LoggerSinkConfiguration loggerSinkConfiguration,
            string host,
-           int port,
+           int port = Port,
            string tag = Tag,
+           int queueLimit = QueueLimit,
+           TimeSpan? batchPeriod = null,
            LogEventLevel restrictedToMinimumLevel = LogEventLevel.Information)
         {
             var sink = new FluentdSink(new FluentdSinkOptions(host, port, tag));
 
-            return loggerSinkConfiguration.Sink(sink, restrictedToMinimumLevel);
+            DefaultBatchingOptions.Period = batchPeriod ?? DefaultPeriod;
+            DefaultBatchingOptions.QueueLimit = queueLimit;
+
+            var batchingSink = new PeriodicBatchingSink(sink, DefaultBatchingOptions);
+            return loggerSinkConfiguration.Sink(batchingSink, restrictedToMinimumLevel);
         }
 
         public static LoggerConfiguration Fluentd(
@@ -39,7 +60,8 @@ namespace Serilog
         {
             var sink = new FluentdSink(new FluentdSinkOptions(udsSocketFilePath));
 
-            return loggerSinkConfiguration.Sink(sink, restrictedToMinimumLevel);
+            var batchingSink = new PeriodicBatchingSink(sink, DefaultBatchingOptions);
+            return loggerSinkConfiguration.Sink(batchingSink, restrictedToMinimumLevel);
         }
     }
 }
--- src/Serilog.Sinks.Fluentd/Sinks/Fluentd/FluentdSink.cs
+++ src/Serilog.Sinks.Fluentd/Sinks/Fluentd/FluentdSink.cs
@@ -1,25 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Serilog.Events;
 using Serilog.Sinks.PeriodicBatching;
 
 namespace Serilog.Sinks.Fluentd
 {
-    public class FluentdSink : PeriodicBatchingSink
+    public class FluentdSink : IBatchedLogEventSink, IDisposable
     {
         private readonly FluentdSinkClient _fluentdClient;
 
-        public FluentdSink(FluentdSinkOptions options) : base(options.BatchPostingLimit, options.Period)
+        public FluentdSink(FluentdSinkOptions options)
         {
             _fluentdClient = new FluentdSinkClient(options);
         }
 
-        protected override async Task EmitBatchAsync(IEnumerable<LogEvent> events)
+        public Task OnEmptyBatchAsync()
         {
-            foreach (var logEvent in events)
+            return Task.CompletedTask;
+        }
+
+        public async Task EmitBatchAsync(IEnumerable<LogEvent> batch)
+        {
+            foreach (var logEvent in batch)
             {
                 await _fluentdClient.SendAsync(logEvent);
             }
+        }
+
+        public void Dispose()
+        {
+            _fluentdClient?.Dispose();
         }
     }
 }
