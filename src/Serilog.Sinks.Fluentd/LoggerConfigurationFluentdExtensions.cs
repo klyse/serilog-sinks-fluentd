40c7f1995bdcfc9f4d1ffadb2e9b5a37751b1086
klyse <klaus.pruenster@gmail.com>
2022-08-02 15:41:58 +0200

    add more configuration options in "writeTo.Fluentd" method

--- src/Serilog.Sinks.Fluentd/LoggerConfigurationFluentdExtensions.cs
+++ src/Serilog.Sinks.Fluentd/LoggerConfigurationFluentdExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Serilog.Configuration;
 using Serilog.Events;
 using Serilog.Sinks.Fluentd;
@@ -11,10 +11,15 @@ namespace Serilog
         private const string Host = "localhost";
         private const int Port = 24224;
         private const string Tag = "Tag";
-
+        private const int QueueLimit = 100000;
+        private static readonly TimeSpan DefaultPeriod = TimeSpan.FromSeconds(2.0);
+        
+        
         private static readonly PeriodicBatchingSinkOptions DefaultBatchingOptions = new PeriodicBatchingSinkOptions
         {
-            BatchSizeLimit = 50
+            BatchSizeLimit = 50,
+            QueueLimit = QueueLimit,
+            Period = DefaultPeriod
         };
         
         public static LoggerConfiguration Fluentd(
@@ -33,11 +38,16 @@ namespace Serilog
         public static LoggerConfiguration Fluentd(
            this LoggerSinkConfiguration loggerSinkConfiguration,
            string host,
-           int port,
+           int port = Port,
            string tag = Tag,
+           int queueLimit = QueueLimit,
+           TimeSpan? batchPeriod = null,
            LogEventLevel restrictedToMinimumLevel = LogEventLevel.Information)
         {
             var sink = new FluentdSink(new FluentdSinkOptions(host, port, tag));
+
+            DefaultBatchingOptions.Period = batchPeriod ?? DefaultPeriod;
+            DefaultBatchingOptions.QueueLimit = queueLimit;
 
             var batchingSink = new PeriodicBatchingSink(sink, DefaultBatchingOptions);
             return loggerSinkConfiguration.Sink(batchingSink, restrictedToMinimumLevel);
